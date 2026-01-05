b14769b90c8e2c31dc97fd96ae38617950371a76
Shannon Chowdhury <refinedzeta@gmail.com>
2026-01-05 10:38:22 -0500

    feat: update location to America/Toronto

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -8,7 +8,7 @@ const person: Person = {
   role: "Product Manager",
   avatar: "/images/avatar.jpg",
   email: "example@gmail.com",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  location: "America/Toronto", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: ["English"], // optional: Leave the array empty if you don't want to display languages
 };
 
